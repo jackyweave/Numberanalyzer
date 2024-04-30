d4914f4d41360a8bcf9cebdca06405e5c45a65ed
jackyweave <71942159+jackyweave@users.noreply.github.com>
2024-04-30 15:18:50 -0400

    inital commit

---
+++ NumberAnalyzer/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+
+
+    
+    
+        int number;
+
+        Console.WriteLine("Hello! Enter a number between 1 and 100: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+     if (number % 2 != 0 && number < 60)
+        {
+            Console.WriteLine($"{number} - Odd and less than 60.");
+        }
+      else if (number % 2 == 0 && number >= 2 && number <= 24)
+        {
+            Console.WriteLine("Even and less than 25.");
+        }
+        else if (number % 2 == 0 && number >= 26 && number <= 60)
+        {
+            Console.WriteLine("Even and between 26 and 60 inclusive.");
+        }
+      else if (number % 2 == 0 && number > 60)
+        {
+            Console.WriteLine($"{number} - Even and greater than 60.");
+        }
+      else if (number % 2 != 0 && number > 60)
+        {
+            Console.WriteLine($"{number} - Odd and greater than 60.");
+        }
+
+if (number <= 0) {
+    Console.WriteLine("Invalid Input (must be between 1 and 100) :/ try again");
+} 
+    
+
+   
+
+
+
+
